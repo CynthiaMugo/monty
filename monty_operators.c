8bd0b1e3f22e32560a6a16c26361f82e3bc1faaa
CynthiaMugo <cynthiamugo25@gmail.com>
2022-10-06 09:19:42 -0700

    Quick fix

--- monty_operators.c
+++ monty_operators.c
@@ -121,7 +121,7 @@ void f_div(stack_t **head, unsigned int counter)
 	h = *head;
 	if (h->n == 0)
 	{
-		fprintf(stderr, "L%d: divisionby zero\n", counter);
+		fprintf(stderr, "L%d: division by zero\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
